20e91a9735bf81203769aca64a4daefb978337ee
mkillewald <mkillewald@yahoo.com>
2020-12-26 03:53:01 -0500

    changed glClearColor to almost black

--- 02 - Lighting/01 - colors/colors.cpp
+++ 02 - Lighting/01 - colors/colors.cpp
@@ -178,7 +178,7 @@ int main()
 
         // render
         // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // create projection and view transformations
