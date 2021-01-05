2ea4bb672bac2592531ac3bc9543bec14e430a60
mkillewald <mkillewald@yahoo.com>
2021-01-05 10:27:17 -0500

    visualizing the depth buffer

--- 04 - Advanced OpenGL /01 - depthTesting/resources/shaders/1.1.depth_testing.fs
+++ 04 - Advanced OpenGL /01 - depthTesting/resources/shaders/1.1.depth_testing.fs
@@ -1,12 +1,22 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec2 TexCoords;
+// in vec2 TexCoords;
 
-uniform sampler2D texture1;
+// uniform sampler2D texture1;
+
+float near = 0.1; 
+float far  = 100.0; 
+  
+float LinearizeDepth(float depth) 
+{
+    float z = depth * 2.0 - 1.0; // back to NDC 
+    return (2.0 * near * far) / (far + near - z * (far - near));	
+}
 
 void main()
-{    
+{             
+    float depth = LinearizeDepth(gl_FragCoord.z) / far; // divide by far for demonstration
+    FragColor = vec4(vec3(depth), 1.0);
     // FragColor = texture(texture1, TexCoords);
-    FragColor = vec4(vec3(gl_FragCoord.z), 1.0);
 }
