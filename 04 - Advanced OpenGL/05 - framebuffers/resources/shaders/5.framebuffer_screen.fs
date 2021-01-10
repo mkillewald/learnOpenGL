4fe28aedb0408c4dbf5f4e6a11356fff6924a8b5
mkillewald <mkillewald@yahoo.com>
2021-01-10 13:23:21 -0500

    post-processing: greyscale

--- 04 - Advanced OpenGL/05 - framebuffers/resources/shaders/5.framebuffer_screen.fs
+++ 04 - Advanced OpenGL/05 - framebuffers/resources/shaders/5.framebuffer_screen.fs
@@ -7,6 +7,9 @@ uniform sampler2D screenTexture;
 
 void main()
 { 
-    // FragColor = texture(screenTexture, TexCoords);
-    FragColor = vec4(vec3(1.0 - texture(screenTexture, TexCoords)), 1.0);
+    FragColor = texture(screenTexture, TexCoords); // original 
+    // FragColor = vec4(vec3(1.0 - texture(screenTexture, TexCoords)), 1.0); // inversion
+
+    float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
+    FragColor = vec4(average, average, average, 1.0); // greyscale
 }
