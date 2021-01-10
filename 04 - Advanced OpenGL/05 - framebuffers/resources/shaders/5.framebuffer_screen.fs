5305d4a7418496da6038d1d2fdebe9e24a7d39e7
mkillewald <mkillewald@yahoo.com>
2021-01-10 13:25:24 -0500

    post-processing: greyscale with weighted channels

--- 04 - Advanced OpenGL/05 - framebuffers/resources/shaders/5.framebuffer_screen.fs
+++ 04 - Advanced OpenGL/05 - framebuffers/resources/shaders/5.framebuffer_screen.fs
@@ -10,6 +10,8 @@ void main()
     FragColor = texture(screenTexture, TexCoords); // original 
     // FragColor = vec4(vec3(1.0 - texture(screenTexture, TexCoords)), 1.0); // inversion
 
-    float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
+    // float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
+    float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b; // weighted channels for more accurate results
+
     FragColor = vec4(average, average, average, 1.0); // greyscale
 }
