8ba15254b9ebe086c01e1b2454a1e170a0ee447e
mkillewald <mkillewald@yahoo.com>
2021-01-10 13:21:31 -0500

    post-processing: color inversion

--- 04 - Advanced OpenGL/05 - framebuffers/resources/shaders/5.framebuffer_screen.fs
+++ 04 - Advanced OpenGL/05 - framebuffers/resources/shaders/5.framebuffer_screen.fs
@@ -7,5 +7,6 @@ uniform sampler2D screenTexture;
 
 void main()
 { 
-    FragColor = texture(screenTexture, TexCoords);
+    // FragColor = texture(screenTexture, TexCoords);
+    FragColor = vec4(vec3(1.0 - texture(screenTexture, TexCoords)), 1.0);
 }
