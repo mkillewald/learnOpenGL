8a4715a5fd4664518cbdd7c1cafeb81d5f9ba598
Mike Killewald <mkillewald@yahoo.com>
2021-01-15 15:17:03 -0500

    added comments

--- 04 - Advanced OpenGL/10 - instancing/instancing.cpp
+++ 04 - Advanced OpenGL/10 - instancing/instancing.cpp
@@ -94,9 +94,9 @@ int main()
     glBindVertexArray(quadVAO);
     glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(0); // aPos
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(1); // aColor
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float),  (void*)(2 * sizeof(float)));
 
     // set instance offset positions
@@ -120,8 +120,7 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * 100, &translations[0], GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    glEnableVertexAttribArray(2);
+    glEnableVertexAttribArray(2); // aOffset
     glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
     glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
     glBindBuffer(GL_ARRAY_BUFFER, 0);	
