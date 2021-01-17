cbc64ecac4bae2cb3b05ecd7e1b8d8f7f715a4be
Mike Killewald <mkillewald@yahoo.com>
2021-01-17 12:41:05 -0500

    small fix

--- 04 - Advanced OpenGL/11 - antiAliasing/resources/shaders/11.aa_post.fs
+++ 04 - Advanced OpenGL/11 - antiAliasing/resources/shaders/11.aa_post.fs
@@ -10,5 +10,5 @@ void main()
     // vec3 col = texture(screenTexture, TexCoords).rgb;
     //float grayscale = 0.2126 * col.r + 0.7152 * col.g + 0.0722 * col.b;
     // FragColor = vec4(vec3(grayscale), 1.0);
-    FragColor = vec4(texture(screenTexture, TexCoords).rgb, 1.0);
+    FragColor = texture(screenTexture, TexCoords);
 }
