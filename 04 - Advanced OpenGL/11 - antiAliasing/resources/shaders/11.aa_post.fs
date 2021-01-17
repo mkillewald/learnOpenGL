d812a71eef9b6da2d5552c9ac7dfb409951e9cd8
Mike Killewald <mkillewald@yahoo.com>
2021-01-17 12:59:25 -0500

    post-processing kernel applied

--- 04 - Advanced OpenGL/11 - antiAliasing/resources/shaders/11.aa_post.fs
+++ 04 - Advanced OpenGL/11 - antiAliasing/resources/shaders/11.aa_post.fs
@@ -5,10 +5,57 @@ in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
 
+const float offset = 1.0 / 300.0; 
+
 void main()
 {
-    // vec3 col = texture(screenTexture, TexCoords).rgb;
-    //float grayscale = 0.2126 * col.r + 0.7152 * col.g + 0.0722 * col.b;
-    // FragColor = vec4(vec3(grayscale), 1.0);
-    FragColor = texture(screenTexture, TexCoords);
+    vec2 offsets[9] = vec2[](
+        vec2(-offset,  offset), // top-left
+        vec2( 0.0f,    offset), // top-center
+        vec2( offset,  offset), // top-right
+        vec2(-offset,  0.0f),   // center-left
+        vec2( 0.0f,    0.0f),   // center-center
+        vec2( offset,  0.0f),   // center-right
+        vec2(-offset, -offset), // bottom-left
+        vec2( 0.0f,   -offset), // bottom-center
+        vec2( offset, -offset)  // bottom-right    
+    );
+
+    // sharpen
+    // float kernel[9] = float[](
+    //     -1, -1, -1,
+    //     -1,  9, -1,
+    //     -1, -1, -1
+    // );
+
+    // blur
+    float kernel[9] = float[](
+        1.0 / 16, 2.0 / 16, 1.0 / 16,
+        2.0 / 16, 4.0 / 16, 2.0 / 16,
+        1.0 / 16, 2.0 / 16, 1.0 / 16  
+    );
+
+    // edge detection
+    // float kernel[9] = float[](
+    //     1,  1,  1,
+    //     1, -8,  1,
+    //     1,  1,  1
+    // );
+    
+    vec3 sampleTex[9];
+    for(int i = 0; i < 9; i++)
+    {
+        sampleTex[i] = vec3(texture(screenTexture, TexCoords.st + offsets[i]));
+    }
+    vec3 col = vec3(0.0);
+    for(int i = 0; i < 9; i++)
+        col += sampleTex[i] * kernel[i];
+    
+    FragColor = vec4(col, 1.0); // kernel effect
+    // FragColor = texture(screenTexture, TexCoords); // original
+    // FragColor = vec4(vec3(1.0 - texture(screenTexture, TexCoords)), 1.0); // inversion
+
+    // float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
+    // float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b; // weighted channels for more accurate results
+    // FragColor = vec4(average, average, average, 1.0); // greyscale
 }
